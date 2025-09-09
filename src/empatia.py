36b8d6cf28dd33b2125a4546290058aa9b79e2c6
convidado <tiago.azekad@gmail.com>
2025-09-09 17:26:05 -0300

    primeira versão do jogo revelando emoções 09/09

---
+++ src/empatia.py
@@ -0,0 +1,6 @@
+from browser import html,document
+texto=input('diga o texto')
+document.body <= html.H1(texto.upper())
+document.body<=html.IMG(src='https://picsum.photos/id/237/200/300')
+sentir=html.DIV('empatia',style={'background-color':'FireBrick','padding':'10px','width':'150px'})
+document.body<=sentir
